d1d595d205c339f1aee189051de5f564e01b646a
RohitSaha46-rr <76219397+RohitSaha46-rr@users.noreply.github.com>
2021-11-19 12:51:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function fun(){
+    window.open("https://www.youtube.com/watch?v=3ISKVTH30nI");
+}
+function facebook(){
+    window.open("https://www.facebook.com/caorunngin");
+
+}
+function insta(){
+    window.open("https://www.instagram.com/CaorunnGin/");
+}
+function twitter(){
+    window.open("https://twitter.com/CaorunnGin");
+}
